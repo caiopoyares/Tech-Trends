0c3beb4124766172f8e5d7aa3cfc8774e40adaed
Caio Macbook <capoyares@gmail.com>
2019-05-30 09:52:48 -0300

    favicon added

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' })
+    new HtmlWebpackPlugin({ template: './src/index.html', favicon: './src/img/favicon.png' })
   ],
-  mode: "production"
+  mode: process.env.NODE_ENV === "production" ? 'production' : 'development'
 }
